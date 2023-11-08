1f9acbcbdcbc933db219a1aca28a786b92f6a437
GustavoIda <111133900+GustavoIda@users.noreply.github.com>
2023-11-08 08:50:21 -0300

    .

--- main.js
+++ main.js
@@ -5,8 +5,13 @@ const listaDeTeclas= document.querySelectorAll(".tecla")
 
 let contador=0;
 while (contador<listaDeTeclas.length){
-
-
+    const efeito=listaDeTeclas[contador].classList[1];
+    const idAudio= '#som_'+efeito;
+    listaDeTeclas [contador].onclick=function(){
+        tocaSom(idAudio);
+    }
+    contador= contador+1;
+    console.log(contador);
 
 
 
